839cf6af01490e04868caa7694bfed8b7757e342
rodrigo.resque <rodrigo.resque@tre-pa.jus.br>
2020-12-23 00:12:18 -0300

    updt

--- lib/main.dart
+++ lib/main.dart
@@ -7,42 +7,47 @@ class CavaleirosApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: ListaCavaleiros(),
-        appBar: AppBar(
-          title: Text('Cavaleiros'),
-          backgroundColor: Colors.amber,
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.amber,
-          child: Icon(Icons.add),
-        ),
+        body: FormularioCavaleiro(),
       ),
     );
   }
 }
 
-
 class ListaCavaleiros extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Cavaleiro(
-          icone: Icons.supervised_user_circle,
-          nome: 'Cavaleiro 1',
-          numeroCasa: 1,
-        ),
-        Cavaleiro(
-          icone: Icons.account_circle,
-          nome: 'Cavaleiro 2',
-          numeroCasa: 2,
-        ),
-        Cavaleiro(
-          icone: Icons.account_circle_outlined,
-          nome: 'Cavaleiro 3',
-          numeroCasa: 3,
-        ),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cavaleiros'),
+        backgroundColor: Colors.amber,
+      ),
+      body: Column(
+        children: [
+          Column(
+            children: [
+              Cavaleiro(
+                icone: Icons.supervised_user_circle,
+                nome: 'Cavaleiro 1',
+                numeroCasa: 1,
+              ),
+              Cavaleiro(
+                icone: Icons.account_circle,
+                nome: 'Cavaleiro 2',
+                numeroCasa: 2,
+              ),
+              Cavaleiro(
+                icone: Icons.account_circle_outlined,
+                nome: 'Cavaleiro 3',
+                numeroCasa: 3,
+              ),
+            ],
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.amber,
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
@@ -58,9 +63,47 @@ class Cavaleiro extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        leading: Icon(icone, size: 48.0,),
+        leading: Icon(
+          icone,
+          size: 48.0,
+        ),
         title: Text(nome),
         subtitle: Text('Numero Casa $numeroCasa'),
+      ),
+    );
+  }
+}
+
+class FormularioCavaleiro extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cadastro de Cavaleiro'),
+        backgroundColor: Colors.amberAccent,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Nome do Cavaleiro',
+                hintText: 'Ex: Seya'
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  labelText: 'Casa do Cavaleiro',
+                  hintText: 'Ex: Seya'
+              ),
+            ),
+          ),
+          RaisedButton(onPressed: null) /* Parei aqui, continuar na aula 03 video 2 (Criando a Transferencia) */
+        ],
       ),
     );
   }
