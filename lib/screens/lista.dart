a861097812cf597e252cb2bd3370edf99b4e3f9c
rodrigo.resque <rodrigo.resque@tre-pa.jus.br>
2021-01-06 13:14:22 -0300

    updt

--- lib/screens/lista.dart
+++ lib/screens/lista.dart
@@ -11,6 +11,33 @@ const _saga =
 const _milo =
     "https://lh5.googleusercontent.com/sKidQM7K6GmUF4fmZR0JvGeYWqJcpg8FS8d3J8NSwx_uhzYf_YAOQrWwN2ps9IsWEBImixkDhvp7KMXX7rf2zkkUi6npbYV9Nxam19eX5Fq38nvxlkoDHglsoLRbKVEDWb_3Hgxe8Q";
 
+const _aldebaran =
+    "https://lh5.googleusercontent.com/ltfaPQU1MYErAaw3JKGwpnt0Hl7FTsGmSIrXY6RJ2XIdmUYb7bgu8FkIgyaV2tKBDzO_RhjwEkKd5N1SnYG6J6rBkJ9Ilwb5c_LaWS5ura4fq3n3JEpgMfzGpYqbeP5mMr7fBpaZlQ";
+
+const _shaka =
+    "https://lh3.googleusercontent.com/NMSC3J8ADtIflzjJGr68yeLAwEYuJiy8XkvfWpQo2MUBIGqQFf-Ldb1HkxjB14ZcuIuuAzARLYmPr_tbh8IiRSm5ZinU5HFAWgK4pKZyr-gSVaZAljiavTcVmCz2DUDw7AOmWADZ8g";
+
+const _shura =
+    "https://lh5.googleusercontent.com/ON0MengxHOtIIpCYllr0nfZuSRHBUHAteHgyJ-KOu0DbN93w84dR7tdPTdYUnTJKkwCN5lAXB-jvl9U_6QbtvbsQvPspcsoK3iEstmHTcRxYfU8lnY2yewmcmax4nhriTCTj0mUfZw";
+
+const _aphrodite =
+    "https://lh6.googleusercontent.com/JUJQtlIbyVC8lhpBXFGN7eGmopYF8-a69dtmafF2xuDCmqzOmiYOEVrg4Q6nDupkGrFbwgCzwraDAt3R8-468boww7baICa1lTqHATekD1qR1arjgLL1b47I24vqxuhg76GFf3KJWA";
+
+const _mu =
+    "https://lh4.googleusercontent.com/TO6EYgN67tg_DdBZZnVb3rG1HxvZG6vAY503xp1LXTpAHfAY3WF9BS_SK28m4khPkRkETRS9Q9tBGrRZ8spRXcMwtUPQ2KdmneZYT_B5aCeIPVZAP22C2FAk0WByIJgMrnkElm1ZqQ";
+
+const _kamus =
+    "https://lh5.googleusercontent.com/GAqfMKdz5IXcmadEQ-gb7UQManQK4C7kCSPaQPE5DGIHDF4pd2rfRk41qzByN24_vAKjY8tbceEInYtpS62zw3Onr8bXNTjWEODipCOdY81OxbwdiVaV9SrGytuhon73N5hmaz-Xxw";
+
+const _dohko =
+    "https://www.cavzodiaco.com.br/imagens-personagens/imagem1/41.png";
+
+const _mascara_da_morte =
+    "https://www.cavzodiaco.com.br/imagens-personagens/imagem1/38.png";
+
+const _aiolia =
+    "https://www.cavzodiaco.com.br/imagens-personagens/imagem1/39.png";
+
 class ListaCavaleiros extends StatefulWidget {
   final List<Cavaleiro> _cavaleiros = List();
 
@@ -71,11 +98,39 @@ class ItemCavaleiro extends StatelessWidget {
       case "aiolos":
         avatar = _aiolos;
         break;
-      case "saga":
+      case ("saga" "kanon"):
         avatar = _saga;
         break;
       case "milo":
         avatar = _milo;
+        break;
+      case "aldebaran":
+        avatar = _aldebaran;
+        break;
+      case "shaka":
+        avatar = _shaka;
+        break;
+      case "shura":
+        avatar = _shura;
+        break;
+      case "aphrodite":
+        avatar = _aphrodite;
+        break;
+      case "mu":
+        avatar = _mu;
+        break;
+      case "kamus":
+        avatar = _kamus;
+        break;
+      case "dohko":
+        avatar = _dohko;
+        break;
+      case "mascara da morte":
+        avatar = _mascara_da_morte;
+        break;
+      case "aiolia":
+        avatar = _aiolia;
+        break;
     }
 
     return Card(
@@ -84,7 +139,9 @@ class ItemCavaleiro extends StatelessWidget {
           backgroundImage: NetworkImage(avatar),
         ),
         title: Text(cavaleiro.nome),
-        subtitle: Text(cavaleiro.numeroCasa.toString() + " " + cavaleiro.armadura.toString()),
+        subtitle: Text("Casa " + cavaleiro.numeroCasa.toString() +
+            " Armadura de " +
+            cavaleiro.armadura.toString()),
       ),
     );
   }
